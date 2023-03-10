2cb5016bf2a2a034fdbf2a9e44563bf2cb7732a9
jnears <jnears@essex.ac.uk>
2023-03-10 11:02:50 +0000

    Add employment sectors

--- js/graph.js
+++ js/graph.js
@@ -1,58 +1,58 @@
-Highcharts.chart('employment-pie', {
-    chart: {
-        type: 'bar'
-    },
-    title: {
-        text: '',
-        align: 'left'
-    },
+// Highcharts.chart('employment-pie', {
+//     chart: {
+//         type: 'bar'
+//     },
+//     title: {
+//         text: '',
+//         align: 'left'
+//     },
     
-    xAxis: {
-        categories: ['Health Professionals','Therapy Professionals','Nursing and Midwifery Professionals'],
-        title: {
-            text: null
-        }
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Migrants (millions)',
-            align: 'high'
-        },
-        labels: {
-            overflow: 'justify'
-        }
-    },
-    tooltip: {
-        valueSuffix: ' millions'
-    },
-    plotOptions: {
-        bar: {
-            dataLabels: {
-                enabled: false
-            }
-        }
-    },
-    // legend: {
-    //     layout: 'vertical',
-    //     align: 'right',
-    //     verticalAlign: 'top',
-    //     x: -40,
-    //     y: 80,
-    //     floating: true,
-    //     borderWidth: 1,
-    //     backgroundColor:
-    //         Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
-    //     shadow: true
-    // },
-    credits: {
-        enabled: false
-    },
-    series: [{
-        name: 'Migrants',
-        data: [47, 12, 59]
-    }]
-});
+//     xAxis: {
+//         categories: ['Education','Health and Social Care','Science and technology'],
+//         title: {
+//             text: null
+//         }
+//     },
+//     yAxis: {
+//         min: 0,
+//         title: {
+//             text: 'Migrants (millions)',
+//             align: 'high'
+//         },
+//         labels: {
+//             overflow: 'justify'
+//         }
+//     },
+//     tooltip: {
+//         valueSuffix: ' millions'
+//     },
+//     plotOptions: {
+//         bar: {
+//             dataLabels: {
+//                 enabled: false
+//             }
+//         }
+//     },
+//     // legend: {
+//     //     layout: 'vertical',
+//     //     align: 'right',
+//     //     verticalAlign: 'top',
+//     //     x: -40,
+//     //     y: 80,
+//     //     floating: true,
+//     //     borderWidth: 1,
+//     //     backgroundColor:
+//     //         Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+//     //     shadow: true
+//     // },
+//     credits: {
+//         enabled: false
+//     },
+//     series: [{
+//         name: 'Migrants',
+//         data: [47, 12, 59]
+//     }]
+// });
 
 
 
@@ -72,7 +72,7 @@ var chart = Highcharts.chart('employment-pie', {
     enabled: false
   },
     title: {
-        text: 'Health and Social Care (20% migrants)'
+        text: '10 Employment sectors'
     },
    
     tooltip: {
@@ -87,33 +87,46 @@ var chart = Highcharts.chart('employment-pie', {
         }
     },
     series: [{
-            name: 'Health and Social Care (20% migrants)',
-        data: [['Health Professionals',47], ['Therapy Professionals',12], ['Nursing and Midwifery Professionals',59]],
+            name: 'All sectors',
+
+            
+
+                    data: [['Arts, culture and media', 20],['Business, law and administration', 20],['Cleaning and security', 20],['Construction and trades', 20],['Education', 20],['Farming and food production', 20],['Health and social care', 20],['Retail and leisure', 20],['Science and technology', 20],['Senior management']],
         colors: ["#f6d491", "#e6b453", "#bf9441", "#997328"],
     }]
 
 });
 
-$('#btnEducation').click(function () {
-    chart.series[0].setData([['Teaching and Educational Professionals',107]]);
-    chart.setTitle({text: "Education Employment (30% migrants)"});
+$('#btnAllSectors').click(function () {
+    chart.series[0].setData([['Arts, culture and media', 20],['Business, law and administration', 20],['Cleaning and security', 20],['Construction and trades', 20],['Education', 20],['Farming and food production', 20],['Health and social care', 20],['Retail and leisure', 20],['Science and technology', 20],['Senior management']]);
+    chart.setTitle({text: "10 Employment sectors"});
+   // chart.xAxis[0].update({categories: ['kkkkkkkk','Therapy Professionals','lllllllll','jjjjjjjj dfgdf','jjjj']}); 
+
 });
 
-$('#btnHealthCare').click(function () {
+$('#btnEducation').click(function () {
+    chart.series[0].setData([['Teaching and Educational Professionals',107]]);
+    chart.setTitle({text: "Education (30% migrants)"});
+    // chart.xAxis[0].update({categories: ['kkkkkkkk','Therapy Professionals','lllllllll','jjjjjjjj dfgdf','jjjj']}); 
+});
+
+$('#btnHealth').click(function () {
     chart.series[0].setData([['Health Professionals',47], ['Therapy Professionals',12], ['Nursing and Midwifery Professionals',59]]);
     chart.setTitle({text: "Health and Social Care (20% migrants)"});
    // chart.xAxis[0].update({categories: ['kkkkkkkk','Therapy Professionals','lllllllll','jjjjjjjj dfgdf','jjjj']}); 
 
 });
 
-$('#btnScienceTechnology').click(function () {
+$('#btnScience').click(function () {
     chart.series[0].setData([['Natural and Social Science Professionals',11], ['Engineering Professionals',11], ['Information Technology and Telecommunications Professionals',60], ['Conservation and Environment Professionals',1], ['Research and Development Managers',1]]);
      chart.setTitle({text: "Science and technology (10% migrants)"});
+     // chart.xAxis[0].update({categories: ['kkkkkkkk','Therapy Professionals','lllllllll','jjjjjjjj dfgdf','jjjj']}); 
 
 });
 
-$('#btnBusinessLawAdministration').click(function () {
+$('#btnBusiness').click(function () {
     chart.series[0].setData([['Business, Research and Administrative Professionals',34], ['Legal Professionals',13], ['Architects, Town Planners and Surveyors',7], ['Welfare Professionals',9], ['Librarians and Related Professionals',2], ['Quality and Regulatory Professionals',9]]);
      chart.setTitle({text: "Business, law and administration (5% migrants)"});
+     // chart.xAxis[0].update({categories: ['kkkkkkkk','Therapy Professionals','lllllllll','jjjjjjjj dfgdf','jjjj']}); 
 
 });
